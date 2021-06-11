177dbbb06e4207e1159d766c1b8c91d502c39d5c
RaulSilvaA <rausilva@ug.uchile.cl>
2021-06-11 18:42:34 -0400

    URL a diccionarios

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://yarnsub.com/yarns/cygnet/chunky'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-tablas = soup.find(class_='details')
-filas = tablas.find_all('tr')
-for fila in filas :
-    print(fila.find('th').text.strip().strip(':'), end=';')
-    print(fila.find('td').text.strip())
-
+file = open('lanas.txt', 'r')
+lineas = file.readlines()
+bbdd = []
+for linea in lineas:
+    URL = linea.strip("\n")
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    tablas = soup.find(class_='details')
+    filas = tablas.find_all('tr')
+    marca = URL.split('/')[-2]
+    lana = URL.split('/')[-1]
+    dic = {'marca': marca, 'lana': lana}
+    data = str(marca)+','+str(lana)
+    for fila in filas :
+        dic[fila.find('th').text.strip().strip(':')] = fila.find('td').text.strip()
+        #dicc.append({fila.find('th').text.strip().strip(':'):fila.find('td').text.strip()})
+        #print(fila.find('th').text.strip().strip(':'), end=';')
+        #print(fila.find('td').text.strip())
+    bbdd.append(dic)
+print(bbdd) 
 '''
 marca = URL.split('/')[-2]
 lana = URL.split('/')[-1]
